835d52cb393a25671bf69c574dfb4700ba6ba914
bete7512 <betekbebe@gmail.com>
2026-07-29 18:51:25 +0300

    anagram check

--- 23-anagram-check/main.go
+++ 23-anagram-check/main.go
@@ -1,23 +1,41 @@
 package main
 
-import "maps"
-
 func main() {}
-
 func IsAnagram(a, b string) bool {
-	arunes := map[rune]int{}
-	brunes := map[rune]int{}
+	counts := map[rune]int{}
 
 	for _, val := range a {
-		arunes[val]++
+		counts[val]++
 	}
 	for _, val := range b {
-		brunes[val]++
+		counts[val]--
 	}
 
-	if maps.Equal(arunes, brunes) {
-		return true
+	for _, val := range counts {
+		if val != 0 {
+			return false
+		}
 	}
 
-	return false
+	return true
+
 }
+
+// naive me
+// func IsAnagram(a, b string) bool {
+// 	arunes := map[rune]int{}
+// 	brunes := map[rune]int{}
+
+// 	for _, val := range a {
+// 		arunes[val]++
+// 	}
+// 	for _, val := range b {
+// 		brunes[val]++
+// 	}
+
+// 	if maps.Equal(arunes, brunes) {
+// 		return true
+// 	}
+
+// 	return false
+// }
